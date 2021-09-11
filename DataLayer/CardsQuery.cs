5fc5a894c8053e5ea718ffe43eb32b3b257fdef2
ekzarov@gmail.com <oekzarov@lohika.com>
2021-09-12 00:43:09 +0300

    - Draft cards query

--- DataLayer/CardsQuery.cs
+++ DataLayer/CardsQuery.cs
@@ -12,15 +12,26 @@ namespace Queries
     {
         private readonly string ConnectionString = ConfigurationManager.ConnectionStrings["CardsDBConnectionString"].ConnectionString;
 
-        const string QUERY = @"SELECT 
-	                             C.Id AS CardID,
-	                             C.[Name],
-                                 C.IsReadOnly,
-	                             CR.ParentId,
-	                             CR.ChildId
-                               FROM Cards C
-	                             LEFT JOIN CardsRelationship CR ON C.Id = CR.ChildId
-                               ORDER BY CR.ParentId";
+        const string GetCardsQuery = @"SELECT 
+	                                     C.Id AS CardID,
+	                                     C.[Name],
+                                         C.InEditMode,
+	                                     CR.ParentId,
+	                                     CR.ChildId
+                                       FROM Cards C
+	                                     LEFT JOIN CardsRelationship CR ON C.Id = CR.ChildId
+                                       ORDER BY CR.ParentId";
+
+        const string GetCardResourcesQuery = @"SELECT 
+	                                             C.Id AS CardID,
+	                                             C.[Name],
+                                                 C.InEditMode,
+	                                             CR.ParentId,
+	                                             CR.ChildId
+                                               FROM Cards C
+	                                             LEFT JOIN CardsRelationship CR ON C.Id = CR.ChildId
+                                               ORDER BY CR.ParentId";
+
         private class CardsRelationship
         {
             public int CardID { get; set; }
@@ -36,13 +47,19 @@ namespace Queries
             using (var connection = new SqlConnection(ConnectionString))
             {
                 await connection.OpenAsync();
-                var cardsRelationships = await connection.QueryAsync<CardsRelationship>(QUERY);
+                var cardsRelationships = await connection.QueryAsync<CardsRelationship>(GetCardsQuery);
 
                 foreach (var item in cardsRelationships)
                 {
                     if (item.ParentId == null)
                     {
                         result.Add(new Card { Id = item.CardID, Title = item.Name, Childs = new List<Card>() });
+
+                        // Work with resources
+                        // var resources = 
+                        // card21.Resources.Add(new TableResource() { Index = 2, Rows = new List<TableResourceItem>() { new TableResourceItem() { Column1 = "Item 1", Column2 = "Item 2" }, new TableResourceItem() { Column1 = "Item 1", Column2 = "Item 2" } } });
+                        // card21.Resources.Add(new ImageResource() { Index = 3, Description = "some text", Uri = "340719-200.png" });
+
                         continue;
                     }
 
@@ -54,6 +71,8 @@ namespace Queries
                             Title = item.Name,
                             InEditMode = item.IsReadOnly // TODO: What for we use this property?
                         });
+
+
                 }
             }
             return result.AsList();
