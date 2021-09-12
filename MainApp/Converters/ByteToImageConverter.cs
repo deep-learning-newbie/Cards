eabc99666de1c0162194349c82407831b39ac944
ekzarov@gmail.com <oekzarov@lohika.com>
2021-09-12 17:50:13 +0300

    - Image as array of bytes

---
+++ MainApp/Converters/ByteToImageConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MainApp.Converters
+{
+    public class ByteToImageConverter : IValueConverter
+    {
+        public BitmapImage ConvertByteArrayToBitMapImage(byte[] byteArray)
+        {
+            var stream = new MemoryStream(byteArray);
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = stream;
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BitmapImage img = new BitmapImage();
+            if (value != null)
+            {
+                img = this.ConvertByteArrayToBitMapImage(value as byte[]);
+            }
+            return img;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
